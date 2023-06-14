c35ec280fca1bf1f112a83b3d0e69a9b1239de05
Esteban <funcesteban@gmail.com>
2023-06-13 21:27:39 -0600

    include project

---
+++ Benchy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace boxing_and_unboxing
+{
+    public class Benchy
+    {
+        private static readonly BoxingService BoxingService = new();
+
+        private static readonly object Boxed = 420;
+
+        [Benchmark] public object BoxValue() => BoxingService.BoxValue(420);
+
+        [Benchmark] public int UnboxValue() => BoxingService.UnboxValue(Boxed);
+
+        [Benchmark] public object SimpleReturnObject() => BoxingService.SimpleReturnObject(Boxed);
+
+        [Benchmark] public int SimpleReturnInt() => BoxingService.SimpleReturnInt(420);
+    }
+
+    public class BoxingService
+    {
+        public object BoxValue(int number)
+        {
+            return number;
+        }
+
+        public int UnboxValue( object value)
+        {
+            return (int)value;
+        }
+
+        public int SimpleReturnInt (int number)
+        {
+            return number;
+        }
+
+        public object SimpleReturnObject (object value)
+        {
+            return value;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using BenchmarkDotNet.Running;
+using boxing_and_unboxing;
+
+Console.WriteLine("Hello, this is an playgroud to see what boxing is and figure out how afeects the memory and the speed.");
+int someNumber = 420;
+//implicity convert to object, this is boxing
+object boxingNumber = someNumber;
+//why is call boxing?
+//When a variable of a *value type* needs to be converted into a *reference type*,
+//an object box is allocated to hold the value, and the value is copied into the box.
+
+//Unboxing is just the opposite.
+//When an object box is cast back to its original value type,
+//the value is copied out of the box and into the appropriate storage location.
+int unboxingNumber = (int)boxingNumber;
+
+
+//let's see an benchmark
+BenchmarkRunner.Run<Benchy>();
+
